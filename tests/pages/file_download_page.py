8b1c48a570f2a27dc0dd0cf72e1e4b205c8467a3
zjtheilen <zjtheilen@gmail.com>
2026-03-03 10:37:12 -0800

    remove unused single file download function

--- tests/pages/file_download_page.py
+++ tests/pages/file_download_page.py
@@ -9,13 +9,6 @@ class FileDownloadPage(BasePage):
     def load(self):
         self.driver.get(self.URL)
 
-    # def download_first_file(self, download_dir):
-    #     file_link = self.wait_for_element(self.FILE_LINK)
-    #     filename = file_link.text
-
-    #     file_link.click()
-    #     return filename
-
     def get_all_filenames(self):
         links = self.wait_for_elements(self.FILE_LINKS)
         return [link.text for link in links]
