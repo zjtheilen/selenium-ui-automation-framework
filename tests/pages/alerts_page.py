ada1bd9ce19ab5975f4657ce744e02f38bc164eb
zjtheilen <zjtheilen@gmail.com>
2026-03-04 09:17:11 -0800

    update and refactor testing for alerts

--- tests/pages/alerts_page.py
+++ tests/pages/alerts_page.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.common.by import By
 from tests.pages.base_page import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class AlertsPage(BasePage):
     URL = "https://the-internet.herokuapp.com/javascript_alerts"
@@ -7,28 +9,42 @@ class AlertsPage(BasePage):
     # Locators
     JS_ALERT_BUTTON = (By.XPATH, "//button[text()='Click for JS Alert']")
     JS_CONFIRM_BUTTON = (By.XPATH, "//button[text()='Click for JS Confirm']")
+    JS_PROMPT_BUTTON = (By.XPATH, "//button[text()='Click for JS Prompt']")
     RESULT_TEXT = (By.ID, "result")
+
+    def __init__(self, driver, timeout=5):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, timeout)
 
     def load(self):
         self.driver.get(self.URL)
 
     def trigger_alert(self):
-        """Click the JS Alert button and accept the alert."""
-        self.wait_for_clickable(self.JS_ALERT_BUTTON).click()
-        alert = self.driver.switch_to.alert
-        alert_text = alert.text
-        alert.accept()
-        result_text = self.wait_for_element(self.RESULT_TEXT).text
-        return alert_text, result_text
+        self.driver.find_element(*self.JS_ALERT_BUTTON).click()
 
     def trigger_confirm(self, accept=True):
-        """Click the JS Confirm button and accept or dismiss the alert."""
-        self.wait_for_clickable(self.JS_CONFIRM_BUTTON).click()
-        alert = self.driver.switch_to.alert
-        alert_text = alert.text
-        if accept:
-            alert.accept()
-        else:
-            alert.dismiss()
-        result_text = self.wait_for_element(self.RESULT_TEXT).text
-        return alert_text, result_text
+        self.driver.find_element(*self.JS_CONFIRM_BUTTON).click()
+    
+    def trigger_prompt(self):
+        self.driver.find_element(*self.JS_PROMPT_BUTTON).click()
+
+    def accept_alert(self):
+        alert = self.wait.until(EC.alert_is_present())
+        alert.accept()
+    
+    def dismiss_alert(self):
+        alert = self.wait.until(EC.alert_is_present())
+        alert.dismiss()
+    
+    def send_text_to_alert(self, text):
+        alert = self.wait.until(EC.alert_is_present())
+        alert.send_keys(text)
+    
+    def get_result_text(self):
+        return self.wait.until(
+            EC.presence_of_element_located(self.RESULT_TEXT)
+        ).text
+    
+    def get_alert_text(self):
+        alert = self.wait.until(EC.alert_is_present())
+        return alert.text
--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -1,22 +1,96 @@
+import pytest
+
 from tests.pages.alerts_page import AlertsPage
+from selenium.common.exceptions import NoAlertPresentException
 
 def test_js_alert(driver):
     page = AlertsPage(driver)
     page.load()
-    alert_text, result_text = page.trigger_alert()
+    
+    page.trigger_alert()
+    alert_text = page.get_alert_text()
+    page.accept_alert()
+    result_text = page.get_result_text()
+
     assert alert_text == "I am a JS Alert"
     assert result_text == "You successfully clicked an alert"
 
-def test_js_confirm_accept(driver):
-    page = AlertsPage(driver)
-    page.load()
-    alert_text, result_text = page.trigger_confirm(accept=True)
-    assert alert_text == "I am a JS Confirm"
-    assert result_text == "You clicked: Ok"
+# def test_js_confirm_accept(driver):
+#     page = AlertsPage(driver)
+#     page.load()
 
-def test_js_confirm_dismiss(driver):
+#     page.trigger_confirm()
+
+#     # alert = driver.switch_to.alert
+#     # alert_text = alert.text
+#     alert_text = page.get_alert_text()
+#     page.accept_alert()
+
+#     # alert.accept()
+
+#     result_text = page.get_result_text()
+
+#     assert alert_text == "I am a JS Confirm"
+#     assert result_text == "You clicked: Ok"
+
+# def test_js_confirm_dismiss(driver):
+#     page = AlertsPage(driver)
+#     page.load()
+
+#     page.trigger_confirm()
+#     alert_text = page.get_alert_text()
+#     page.dismiss_alert()
+
+#     # alert = driver.switch_to.alert
+#     # alert_text = alert.text
+
+#     # alert.dismiss()
+#     result_text = page.get_result_text()
+
+#     # result_text = page.get_result_text()
+
+#     assert alert_text == "I am a JS Confirm"
+#     assert result_text == "You clicked: Cancel"
+
+@pytest.mark.parametrize(
+    "action, expected_text",
+    [
+        ("accept", "You clicked: Ok"),
+        ("dismiss", "You clicked: Cancel"),
+    ]
+)
+def test_js_confirm_variations(driver, action, expected_text):
     page = AlertsPage(driver)
     page.load()
-    alert_text, result_text = page.trigger_confirm(accept=False)
-    assert alert_text == "I am a JS Confirm"
-    assert result_text == "You clicked: Cancel"
+    page.trigger_confirm()
+
+    if action == "accept":
+        page.accept_alert()
+    else:
+        page.dismiss_alert()
+    
+    assert page.get_result_text() == expected_text
+
+@pytest.mark.parametrize(
+    "input_text, expected",
+    [
+        ("Hello", "You entered: Hello"),
+        ("", "You entered:"),
+    ],
+)
+def test_js_prompt_variations(driver, input_text, expected):
+    page = AlertsPage(driver)
+    page.load()
+    page.trigger_prompt()
+
+    page.send_text_to_alert(input_text)
+    page.accept_alert()
+
+    assert page.get_result_text() == expected
+
+def test_alert_not_present(driver):
+    page = AlertsPage(driver)
+    page.load()
+
+    with pytest.raises(NoAlertPresentException):
+        driver.switch_to.alert.accept()
