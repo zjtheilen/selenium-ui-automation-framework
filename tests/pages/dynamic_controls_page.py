40d10f3bd97d3a3402a86a350c283fccb850b9e6
zjtheilen <zjtheilen@gmail.com>
2026-03-03 16:47:08 -0800

    add negative testing for dynamic controls

--- tests/pages/dynamic_controls_page.py
+++ tests/pages/dynamic_controls_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from tests.pages.base_page import BasePage
 
 class DynamicControlsPage(BasePage):
@@ -16,3 +17,16 @@ class DynamicControlsPage(BasePage):
         self.wait_for_clickable(self.ENABLE_BUTTON).click()
         self.wait_for_element(self.MESSAGE)
         return self.wait_for_element(self.INPUT_FIELD).is_enabled()
+    
+    def enter_text_in_input(self, text):
+        input_field = self.wait.until(
+            EC.presence_of_element_located(self.INPUT_FIELD)
+        )
+        input_field.send_keys(text)
+    
+    def test_input_starts_disabled(driver):
+        page = DynamicControlsPage(driver)
+        page.load()
+
+        input_field = page.get_input_element()
+        assert not input_field.is_enabled()
---
+++ tests/test_dynamic_controls_negative.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.common.exceptions import ElementNotInteractableException
+
+from tests.pages.dynamic_controls_page import DynamicControlsPage
+
+def test_enable_input_negative(driver):
+    page = DynamicControlsPage(driver)
+    page.load()
+
+    with pytest.raises(ElementNotInteractableException):
+        page.enter_text_in_input("text")
