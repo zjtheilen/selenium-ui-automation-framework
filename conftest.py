14ecc1f9676d06986dbd24131ee6bf97f0b5deb1
zjtheilen <zjtheilen@gmail.com>
2026-03-05 12:21:47 -0800

    add screenshot logic for failed tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import os
 import shutil
-
 import pytest
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 DOWNLOAD_DIR = os.path.abspath("downloads")
+SCREENSHOTS_DIR = os.path.abspath("screenshots")
 
 
 @pytest.fixture
@@ -45,3 +47,21 @@ def driver(tmp_path):
     yield driver
 
     driver.quit()
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+
+    outcome = yield
+    rep = outcome.get_result()
+
+    if rep.when == "call" and rep.failed:
+        driver = item.funcargs.get("driver")
+        if driver is not None:
+            if not os.path.exists(SCREENSHOTS_DIR):
+                os.makedirs(SCREENSHOTS_DIR)
+
+            file_name = f"{item.name}_{int(time.time())}.png"
+            path = os.path.join(SCREENSHOTS_DIR, file_name)
+            driver.save_screenshot(path)
+            print(f"\nScreenshot saved to {path}")
